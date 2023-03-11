ebb810286474594f0499d44d8355dfcffb0abacb
bluesimp1102 <jackismyname7@gmail.com>
2023-03-11 14:06:53 -0800

    asd

--- pa2/find_kth_smallest.py
+++ pa2/find_kth_smallest.py
@@ -1,6 +1,6 @@
 import random
 
-def select_kth_smallest_1(k, S):
+def find_kth_smallest_1(k, S):
     n = len(S)
     if n == 1:
         return S[0]
@@ -10,13 +10,13 @@ def select_kth_smallest_1(k, S):
         S2 = [x for x in S if x == m]
         S3 = [x for x in S if x > m]
         if len(S1) >= k:
-            return select_kth_smallest_1(k, S1)
+            return find_kth_smallest_1(k, S1)
         elif len(S1) + len(S2) >= k:
             return m
         else:
-            return select_kth_smallest_1(k - len(S1) - len(S2), S3)
+            return find_kth_smallest_1(k - len(S1) - len(S2), S3)
 
-def select_kth_smallest_2(k, S):
+def find_kth_smallest_2(k, S):
     n = len(S)
     if n < 50:
         # sort the list and return the kth smallest element
@@ -28,18 +28,18 @@ def select_kth_smallest_2(k, S):
         # find the medians of the sublists
         medians = [sublist[2] for sublist in sublists if len(sublist) >= 3]
         # find the median of the medians
-        m = select_kth_smallest_2(len(medians) // 2, medians)
+        m = find_kth_smallest_2(len(medians) // 2, medians)
         # partition S into S1, S2, and S3
         S1 = [x for x in S if x < m]
         S2 = [x for x in S if x == m]
         S3 = [x for x in S if x > m]
         # recursively call the algorithm on the appropriate partition
         if len(S1) >= k:
-            return select_kth_smallest_2(k, S1)
+            return find_kth_smallest_2(k, S1)
         elif len(S1) + len(S2) >= k:
             return m
         else:
-            return select_kth_smallest_2(k - len(S1) - len(S2), S3)      
+            return find_kth_smallest_2(k - len(S1) - len(S2), S3)      
               
 def main():
     import time
@@ -51,13 +51,13 @@ def main():
     print(S)  # print the original set
     # find the 5th smallest integer using the first algorithm
     start_time = time.time()
-    result1 = select_kth_smallest_1(k, S)
+    result1 = find_kth_smallest_1(k, S)
     stop_time = time.time()
     print(f"First algorithm:\nThe {k}th smallest integer is {result1} (CPU time: {stop_time-start_time:.6f}s)")
 
     # find the 5th smallest integer using the second algorithm
     start_time = time.time()
-    result2 = select_kth_smallest_2(k, S)
+    result2 = find_kth_smallest_2(k, S)
     stop_time = time.time()
     print(f"Second algorithm:\nThe {k}th smallest integer is {result2} (CPU time: {stop_time-start_time:.6f}s)")
 
